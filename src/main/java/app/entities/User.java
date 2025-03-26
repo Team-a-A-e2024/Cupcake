0e5e900f530c1862f33deec92594d85bd42e6a0e
Iirne <emil@caerli.dk>
2025-03-26 12:41:25 +0100

    added user entity

--- src/main/java/app/entities/User.java
+++ src/main/java/app/entities/User.java
@@ -2,11 +2,11 @@ package app.entities;
 
 public class User {
 
-    int id;
-    String email;
-    String password;
-    String role;
-    double credit;
+    private int id;
+    private String email;
+    private String password;
+    private String role;
+    private double credit;
 
     public User(int id, String email, String password, String role, double credit) {
         this.id = id;
@@ -19,6 +19,48 @@ public class User {
         this.email = email;
         this.password = password;
         this.role = role;
+        this.credit = credit;
+    }
+
+    //getter and setters from here on
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getRole() {
+        return role;
+    }
+
+    public void setRole(String role) {
+        this.role = role;
+    }
+
+    public double getCredit() {
+        return credit;
+    }
+
+    public void setCredit(double credit) {
         this.credit = credit;
     }
 }
