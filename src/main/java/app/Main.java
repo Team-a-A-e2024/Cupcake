162a2500a5bb7560520c6faa6642b9ce38c21f17
Frederik-BipBop <Bastiansen44@gmail.com>
2025-03-27 12:04:00 +0100

    - Har lavet et login html - har lavet en user klasse - har lavet en usermapper der connecter med databasen og controller - Har lavet en controller - lavet en test til usermapper

--- src/main/java/app/Main.java
+++ src/main/java/app/Main.java
@@ -12,7 +12,6 @@ import io.javalin.rendering.template.JavalinThymeleaf;
 import java.util.logging.Logger;
 
 public class Main {
-    private static final Logger LOGGER = Logger.getLogger(Main.class.getName());
 
     private static final String USER = "postgres";
     private static final String PASSWORD = "postgres";
