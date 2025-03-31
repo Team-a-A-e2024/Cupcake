3d749ff58f9666c92954ae25780d14cf6edf5a79
Frederik-BipBop <Bastiansen44@gmail.com>
2025-04-01 00:35:59 +0200

    - Ændret så når du laver en bruger så starter du med 0 credits

--- src/main/java/app/controllers/SignupController.java
+++ src/main/java/app/controllers/SignupController.java
@@ -21,8 +21,7 @@ public class SignupController {
     public static void SignupPost(@NotNull Context ctx) {
         String email = ctx.formParam("email");
         String password = ctx.formParam("password");
-        //todo: default value for credit
-        User user = new User(email,password,null,200);
+        User user = new User(email,password,null,0);
         try{
             if(UsersMapper.getUserByEmail(email) == null){
                 user = UsersMapper.addUserByObject(user);
